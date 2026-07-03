5f95dc8b6ddbe5f795bf616e0d7bc1e586fe9193
Sheriff-Unit-3 <210896603+Sheriff-Unit-3@users.noreply.github.com>
2026-07-03 11:55:22 -0500

    minetest --> core

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 -- [MOD] Angled Walls [angledwalls] [1.3] by TumeniNodes 6-24-2016
 
-dofile(minetest.get_modpath("angledwalls").."/angledwalls.lua")
-dofile(minetest.get_modpath("angledwalls").."/angledglass.lua")
-dofile(minetest.get_modpath("angledwalls").."/adoors.lua")
-dofile(minetest.get_modpath("angledwalls").."/slopedwalls.lua")
+dofile(core.get_modpath("angledwalls").."/src/angledwalls.lua")
+dofile(core.get_modpath("angledwalls").."/src/angledglass.lua")
+dofile(core.get_modpath("angledwalls").."/src/adoors.lua")
+dofile(core.get_modpath("angledwalls").."/src/slopedwalls.lua")
 
-if minetest.get_modpath("bakedclay") then
-	dofile(minetest.get_modpath("angledwalls").."/bakedclay.lua")
+if core.get_modpath("bakedclay") then
+	dofile(core.get_modpath("angledwalls").."/src/bakedclay.lua")
 end
 
-if minetest.get_modpath("quartz") then
-	dofile(minetest.get_modpath("angledwalls").."/quartz.lua")
+if core.get_modpath("quartz") then
+	dofile(core.get_modpath("angledwalls").."/src/quartz.lua")
 end
