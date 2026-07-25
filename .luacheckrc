Sheriff-Unit-3/angledwalls

commit 5f95dc8b6ddbe5f795bf616e0d7bc1e586fe9193

minetest --> core

adoors.lua
@@ -46,7 +46,7 @@ for _, row in ipairs(adoors.door) do
 	local craft_material = row[7]
 
 
-minetest.register_node(":adoors:" ..name.. "_Ldoor", {
+core.register_node(":adoors:" ..name.. "_Ldoor", {
 	description = desc.. " Door (left)",
 	inventory_image = "doors_item_" ..name.. ".png^[transformFXX",
 	wield_image = "doors_item_" ..name.. ".png^[transformFXX",
@@ -88,12 +88,12 @@ minetest.register_node(":adoors:" ..name.. "_Ldoor", {
 		},
 	},
 	on_rightclick = function(pos, node, puncher)
-		minetest.swap_node(pos, {name = "adoors:" ..name.. "_Ldoor_open", param2 = node.param2})
-		minetest.sound_play(door_sound.."_open", {gain = 0.20, max_hear_distance = 2})
+		core.swap_node(pos, {name = "adoors:" ..name.. "_Ldoor_open", param2 = node.param2})
+		core.sound_play(door_sound.."_open", {gain = 0.20, max_hear_distance = 2})
 	end,
 })
 
-minetest.register_node(":adoors:" ..name.. "_Ldoor_open", {
+core.register_node(":adoors:" ..name.. "_Ldoor_open", {
 	drawtype = "mesh",
 	mesh = "adoors_Ldoor_open.obj",
 	tiles = {door_tiles},
@@ -134,12 +134,12 @@ minetest.register_node(":adoors:" ..name.. "_Ldoor_open", {
 		},
 	},
 	on_rightclick = function(pos, node, puncher)
-		minetest.swap_node(pos, {name = "adoors:" ..name.. "_Ldoor", param2 = node.param2})
-		minetest.sound_play(door_sound.."_close", {gain = 0.20, max_hear_distance = 2})
+		core.swap_node(pos, {name = "adoors:" ..name.. "_Ldoor", param2 = node.param2})
+		core.sound_play(door_sound.."_close", {gain = 0.20, max_hear_distance = 2})
 	end,
 })
 
-minetest.register_node(":adoors:" ..name.. "_Rdoor", {
+core.register_node(":adoors:" ..name.. "_Rdoor", {
 	description = desc.. " Door (right)",
 	inventory_image = "doors_item_" ..name.. ".png",
 	wield_image = "doors_item_" ..name.. ".png",
@@ -181,12 +181,12 @@ minetest.register_node(":adoors:" ..name.. "_Rdoor", {
 		},
 	},
 	on_rightclick = function(pos, node, puncher)
-		minetest.swap_node(pos, {name = "adoors:" ..name.. "_Rdoor_open", param2 = node.param2})
-		minetest.sound_play(door_sound.."_open", {gain = 0.20, max_hear_distance = 2})
+		core.swap_node(pos, {name = "adoors:" ..name.. "_Rdoor_open", param2 = node.param2})
+		core.sound_play(door_sound.."_open", {gain = 0.20, max_hear_distance = 2})
 	end,
 })
 
-minetest.register_node(":adoors:" ..name.. "_Rdoor_open", {
+core.register_node(":adoors:" ..name.. "_Rdoor_open", {
 	drawtype = "mesh",
 	mesh = "adoors_Rdoor_open.obj",
 	tiles = {door_tiles},
@@ -227,8 +227,8 @@ minetest.register_node(":adoors:" ..name.. "_Rdoor_open", {
 		},
 	},
 	on_rightclick = function(pos, node, puncher)
-		minetest.swap_node(pos, {name = "adoors:" ..name.. "_Rdoor", param2 = node.param2})
-		minetest.sound_play(door_sound.."_close", {gain = 0.20, max_hear_distance = 2})
+		core.swap_node(pos, {name = "adoors:" ..name.. "_Rdoor", param2 = node.param2})
+		core.sound_play(door_sound.."_close", {gain = 0.20, max_hear_distance = 2})
 	end,
 })
 

angledglass.lua
@@ -20,7 +20,7 @@ function angledglass.angled_place(itemstack, placer, pointed_thing)
 			param2 = 3
 		end
 	end
-	return minetest.item_place(itemstack, placer, pointed_thing, param2)
+	return core.item_place(itemstack, placer, pointed_thing, param2)
 end
 
 
@@ -30,7 +30,7 @@ end
 function angledglass.register_glass(subname, recipeitem, groups, images, description, sounds)
 	groups.glass = 1
 
-minetest.register_node(":angledglass:glass" .. subname, {
+core.register_node(":angledglass:glass" .. subname, {
 	description = description,
 	drawtype = "mesh",
 	mesh = "angled_glass.obj",
@@ -74,7 +74,7 @@ angledglass.register_glass("_acacia_wood_glass", "default:acacia_wood",
 		{"default_glass.png", "default_acacia_wood.png"},
 		"Acacia Wood Glass",
 		default.node_sound_glass_defaults())
-		
+
 angledglass.register_glass("_acacia_wood_obsidian_glass", "default:acacia_wood",
 		{choppy = 2, oddly_breakable_by_hand = 2, flammable = 3, wood = 1},
 		{"default_obsidian_glass.png", "default_acacia_wood.png"},

init.lua
@@ -1,14 +1,14 @@
 -- [MOD] Angled Walls [angledwalls] [1.3] by TumeniNodes 6-24-2016
 
-dofile(minetest.get_modpath("angledwalls").."/angledwalls.lua")
-dofile(minetest.get_modpath("angledwalls").."/angledglass.lua")
-dofile(minetest.get_modpath("angledwalls").."/adoors.lua")
-dofile(minetest.get_modpath("angledwalls").."/slopedwalls.lua")
+dofile(core.get_modpath("angledwalls").."/src/angledwalls.lua")
+dofile(core.get_modpath("angledwalls").."/src/angledglass.lua")
+dofile(core.get_modpath("angledwalls").."/src/adoors.lua")
+dofile(core.get_modpath("angledwalls").."/src/slopedwalls.lua")
 
-if minetest.get_modpath("bakedclay") then
-	dofile(minetest.get_modpath("angledwalls").."/bakedclay.lua")
+if core.get_modpath("bakedclay") then
+	dofile(core.get_modpath("angledwalls").."/src/bakedclay.lua")
 end
 
-if minetest.get_modpath("quartz") then
-	dofile(minetest.get_modpath("angledwalls").."/quartz.lua")
+if core.get_modpath("quartz") then
+	dofile(core.get_modpath("angledwalls").."/src/quartz.lua")
 end

angledwalls.lua
@@ -22,7 +22,7 @@ function angledwalls.angled_place(itemstack, placer, pointed_thing)
 			param2 = 3
 		end
 	end
-	return minetest.item_place(itemstack, placer, pointed_thing, param2)
+	return core.item_place(itemstack, placer, pointed_thing, param2)
 end
 
 --Register angledwalls.
@@ -30,7 +30,7 @@ end
 
 function angledwalls.register_angled_wall(subname, recipeitem, groups, images, description, sounds)
 	groups.angledwall = 1
-minetest.register_node(":angledwalls:angled_wall" .. subname, {
+core.register_node(":angledwalls:angled_wall" .. subname, {
 	description = description,
 	drawtype = "mesh",
 	mesh = "angled_wall.obj",
@@ -64,7 +64,7 @@ end
 
 function angledwalls.register_low_angled_wall(subname, recipeitem, groups, images, description, sounds)
 	groups.lowangledwall = 1
-minetest.register_node(":angledwalls:low_angled_wall" .. subname, {
+core.register_node(":angledwalls:low_angled_wall" .. subname, {
 	description = description,
 	drawtype = "mesh",
 	mesh = "low_angled_wall.obj",
@@ -102,7 +102,7 @@ end
 
 function angledwalls.register_corner(subname, recipeitem, groups, images, description, sounds)
 	groups.corner = 1
-minetest.register_node(":angledwalls:corner" .. subname, {
+core.register_node(":angledwalls:corner" .. subname, {
 	description = description,
 	drawtype = "mesh",
 	mesh = "angledwalls_corner.obj",

slopedwalls.lua
@@ -7,7 +7,7 @@ slopedwalls = {}
 
 function slopedwalls.register_sloped_wall(subname, recipeitem, groups, images, description, sounds)
 groups.sloped = 1
-minetest.register_node(":slopedwalls:sloped_wall" .. subname, {
+core.register_node(":slopedwalls:sloped_wall" .. subname, {
 	description = description,
 	drawtype = "mesh",
 	mesh = "sloped_wall.obj",
@@ -192,7 +192,7 @@ slopedwalls.register_sloped_wall("_stonebrick", "default:stonebrick",
 		{"default_stone_brick.png"},
 		"Stone Brick Sloped Wall",
 		default.node_sound_stone_defaults())
-		
+
 slopedwalls.register_sloped_wall("_obsidian", "default:obsidian",
 		{cracky = 1, level = 2},
 		{"default_obsidian.png"},

commit e7e40730afaba8d966e2bd9b3bfb1fd4a1530856

Fix error caused by 5f95dc8 and fix deprecation warnings

.github/workflows/luacheck.yml
@@ -1,0 +1,10 @@
+name: luacheck
+on: [push, pull_request]
+jobs:
+  luacheck:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@main
+      - name: Luacheck
+        uses: lunarmodules/luacheck@master

.luacheckrc
@@ -1,0 +1,22 @@
+unused_args = false
+allow_defined_top = true
+max_line_length = 170
+
+globals = {
+    "core",
+    "angledglass",
+    "angledwalls",
+    "slopedwalls",
+}
+
+read_globals = {
+    string = {fields = {"split"}},
+    table = {fields = {"copy", "getn"}},
+
+    -- Builtin
+    "vector", "ItemStack",
+    "dump",
+
+    -- MTG
+    "default"
+}

adoors.lua
@@ -47,6 +47,8 @@ adoors.door = {
 	},
 }
 
+local ALPHA_CLIP = core.features.use_texture_alpha_string_modes and "clip" or true
+
 for _, row in ipairs(adoors.door) do
 	local name = row[1]
 	local desc = row[2]
@@ -64,7 +66,7 @@ core.register_node(":adoors:" ..name.. "_Ldoor", {
 	drawtype = "mesh",
 	mesh = "adoors_Ldoor.obj",
 	tiles = {door_tiles},
-	use_texture_alpha = true,
+	use_texture_alpha = ALPHA_CLIP,
 	paramtype = "light",
 	paramtype2 = "facedir",
 	on_rotate = screwdriver.rotate_simple,
@@ -108,7 +110,7 @@ core.register_node(":adoors:" ..name.. "_Ldoor_open", {
 	drawtype = "mesh",
 	mesh = "adoors_Ldoor_open.obj",
 	tiles = {door_tiles},
-	use_texture_alpha = true,
+	use_texture_alpha = ALPHA_CLIP,
 	paramtype = "light",
 	paramtype2 = "facedir",
 	on_rotate = screwdriver.rotate_simple,
@@ -157,7 +159,7 @@ core.register_node(":adoors:" ..name.. "_Rdoor", {
 	drawtype = "mesh",
 	mesh = "adoors_Rdoor.obj",
 	tiles = {door_tiles},
-	use_texture_alpha = true,
+	use_texture_alpha = ALPHA_CLIP,
 	paramtype = "light",
 	paramtype2 = "facedir",
 	on_rotate = screwdriver.rotate_simple,
@@ -201,7 +203,7 @@ core.register_node(":adoors:" ..name.. "_Rdoor_open", {
 	drawtype = "mesh",
 	mesh = "adoors_Rdoor_open.obj",
 	tiles = {door_tiles},
-	use_texture_alpha = true,
+	use_texture_alpha = ALPHA_CLIP,
 	paramtype = "light",
 	paramtype2 = "facedir",
 	on_rotate = screwdriver.rotate_simple,

angledglass.lua
@@ -26,6 +26,8 @@ end
 --Register angledglass.
 --Node will be called angledglass:glass_<subname>
 
+local ALPHA_CLIP = core.features.use_texture_alpha_string_modes and "clip" or true
+
 function angledglass.register_glass(subname, recipeitem, groups, images, description, sounds)
 	groups.glass = 1
 
@@ -34,7 +36,7 @@ function angledglass.register_glass(subname, recipeitem, groups, images, descrip
 		drawtype = "mesh",
 		mesh = "angled_glass.obj",
 		tiles = images,
-		use_texture_alpha = true,
+		use_texture_alpha = ALPHA_CLIP,
 		paramtype = "light",
 		sunlight_propogates = true,
 		paramtype2 = "facedir",

init.lua
@@ -1,14 +1,16 @@
 -- [MOD] Angled Walls [angledwalls] [1.3] by TumeniNodes 6-24-2016
 
-dofile(core.get_modpath("angledwalls").."/src/angledwalls.lua")
-dofile(core.get_modpath("angledwalls").."/src/angledglass.lua")
-dofile(core.get_modpath("angledwalls").."/src/adoors.lua")
-dofile(core.get_modpath("angledwalls").."/src/slopedwalls.lua")
+local modpath = core.get_modpath("angledwalls")
+
+dofile(modpath .. "/angledwalls.lua")
+dofile(modpath .. "/angledglass.lua")
+dofile(modpath .. "/adoors.lua")
+dofile(modpath .. "/slopedwalls.lua")
 
 if core.get_modpath("bakedclay") then
-	dofile(core.get_modpath("angledwalls").."/src/bakedclay.lua")
+	dofile(modpath .. "/bakedclay.lua")
 end
 
 if core.get_modpath("quartz") then
-	dofile(core.get_modpath("angledwalls").."/src/quartz.lua")
+	dofile(modpath .. "/quartz.lua")
 end